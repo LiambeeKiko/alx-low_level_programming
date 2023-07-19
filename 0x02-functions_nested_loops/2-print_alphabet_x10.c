849a31fb1b395b8878e2c8c45e20b56fd1307990
LiambeeKiko <agedavid7@gmail.com>
2023-07-19 06:15:15 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
  *
  * Return: 0, success.
  */
+
 void print_alphabet_x10(void)
 {
 	int i;
 	char a;
 
-	while(i < 10)
+	for (i = 1; i <= 10; i++)
 	{
 		for (a = 'a'; a <= 'z'; a++)
 			_putchar(a);
 		_putchar('\n');
-		i++;
 	}
 }
