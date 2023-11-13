1991d8061b19e92d11170a59e5c4f8d904091a27
LiambeeKiko <agedavid7@gmail.com>
2023-11-13 07:49:56 -0800

    task 001

---
+++ 0x18-dynamic_libraries/100-atoi.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _atoi - Convert a string to integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
+ */
+
+int _atoi(void)
+{
+	/* int x; */
+	/* char arr[]; */
+
+	/* while (*s) */
+	/* { */
+	/* x++; */
+	/* if (*s >= 48 && *s <= '9') */
+	/* conv[i] = *s */
+	/* } */
+
+	return (0);
+}
---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs- computes absolute value of an integer
+ * @a : running values
+ * Return: success 0.
+ */
+int _abs(int a)
+{
+
+	if (a < 0)
+		return (a * -1);
+	return (a);
+}
